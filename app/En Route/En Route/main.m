2f1d56c9b31135f1769ff0eb59604a531d5aa29e
Your Name Herequintendelahaye <quinten_delahaye@hotmail.com>
2014-06-01 15:47:07 +0200

    Initial Commit

---
+++ app/En Route/En Route/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  En Route
+//
+//  Created by Quinten Delahaye on 01/06/14.
+//  Copyright (c) 2014 Quinten Delahaye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ app/En Route/En RouteTests/En_RouteTests.m
@@ -0,0 +1,34 @@
+//
+//  En_RouteTests.m
+//  En RouteTests
+//
+//  Created by Quinten Delahaye on 01/06/14.
+//  Copyright (c) 2014 Quinten Delahaye. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface En_RouteTests : XCTestCase
+
+@end
+
+@implementation En_RouteTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
