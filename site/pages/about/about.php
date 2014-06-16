4b67e6cc0b3c88f745346dd7a3725083abbbc969
zoeVk <zoe.van.kuyk@student.howest.be>
2014-06-16 22:56:24 +0200

    website updated

--- site/pages/about/about.php
+++ site/pages/about/about.php
@@ -8,7 +8,7 @@
             <h3>over en route</h3>
             <p>Hieronder staat ons promofimpje dat kort verteld wat we doen!</p>
         </header>
-        <iframe src="http://player.vimeo.com/video/67017187" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+        <iframe src="http://player.vimeo.com/video/98362321" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
     </section>
     <a href="index.php?page=subscribe" class="btnSubscribe"><span>inschrijven</span></a>
     <div class="peetje" data-0="left:-106px; opacity:1" data-300="left:1300px; opacity:0">&nbsp;</div>
